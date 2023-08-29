e863b5b104a85c8f41273aa93c629e9042c62f96
Gggplso(Company) <gggplso@foxmail.com>
2023-08-29 14:00:14 +0800

    添加网络是否连接的判断

--- BingImageDownload/Program.cs
+++ BingImageDownload/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BingImageDownload.Classes;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace BingImageDownload
@@ -14,18 +15,22 @@ namespace BingImageDownload
             try
             {
                 Console.Title = "Bing每日图片下载(仅限用作桌面壁纸)";
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
                 if (!File.Exists(ShareClass._iniFilePath))
                 {
                     Console.WriteLine("未找到配置文件，开始初始化程序……");
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder.AppendLine("【版权】仅限于壁纸使用。它们是受版权保护的图像，因此您不应将其用于其他目的，但可以将其用作桌面壁纸。");
-                    stringBuilder.AppendLine("【说明】\nPixelResolution=UHD表示下载高分辨率的图片，比如3840x2160，其他值为默认的，比如1920x1080。\nFileNameLanguageIsEnglish=false表示文件名用中文，=true表示文件名用英文。\nDownloadPath=设置保存的路径，不配置则保存到程序所在目录。\nAutoExit=1表示程序运行完自动退出。ExitTime=3000表示退出时等待时间为3秒。");
+                    stringBuilder.AppendLine("【说明】\nPixelResolution=UHD表示下载高分辨率的图片，比如3840x2160，其他值为默认的，比如1920x1080。\nFileNameLanguageIsEnglish=false表示文件名用中文，=true表示文件名用英文。\nDownloadPath=设置保存的路径，不配置则保存到程序所在目录。\nNetWaitTime=2000表示若网络中断尝试重新连接等待的时间为2秒。NetRetryCount=5表示连接网络最大重试次数为5次。\nAutoExit=1表示程序运行完自动退出。ExitTime=3000表示退出时等待时间为3秒。");
                     stringBuilder.AppendLine("【辅助】Win11系统添加到开机启动项：\n在本程序文件BingImageDownload.exe点击右键-发送到桌面快捷方式。\n在系统开始菜单上点击右键-运行，输入shell:startup回车确定系统自动打开一文件夹：开始菜单-程序-启动.\\Start Menu\\Programs\\Startup\n将刚才桌面上创建的快捷方式拖入到此文件夹中即可。");
                     stringBuilder.AppendLine();
                     stringBuilder.AppendLine("[BingApiUrl]");
-                    stringBuilder.AppendLine("url1=https://www.bing.com/HPImageArchive.aspx?format=js&cc=cn&idx=");
-                    stringBuilder.AppendLine("url2=&n=");
-                    stringBuilder.AppendLine("url3=&video=1");
+                    stringBuilder.AppendLine("url1=https://");
+                    stringBuilder.AppendLine("url2=www.bing.com");
+                    stringBuilder.AppendLine("url3=/HPImageArchive.aspx?format=js&cc=cn&idx=");
+                    stringBuilder.AppendLine("url4=&n=");
+                    stringBuilder.AppendLine("url5=&video=1");
                     stringBuilder.AppendLine("DaysAgo=0");
                     stringBuilder.AppendLine("AFewDays=1");
                     stringBuilder.AppendLine();
@@ -33,6 +38,10 @@ namespace BingImageDownload
                     stringBuilder.AppendLine("PixelResolution=UHD");
                     stringBuilder.AppendLine("FileNameLanguageIsEnglish=false");
                     stringBuilder.AppendLine("DownloadPath=");
+                    stringBuilder.AppendLine();
+                    stringBuilder.AppendLine("[NetworkInformation]");
+                    stringBuilder.AppendLine("NetWaitTime=2000");
+                    stringBuilder.AppendLine("NetRetryCount=5");
                     stringBuilder.AppendLine();
                     stringBuilder.AppendLine("[OtherSetting]");
                     stringBuilder.AppendLine("AutoExit=1");
@@ -71,18 +80,19 @@ namespace BingImageDownload
                 {
                     intN = 1;
                 }
-                string strUrl = IniHelper.IniRead("BingApiUrl", "url1", ShareClass._iniFilePath, "https://www.bing.com/HPImageArchive.aspx?format=js&cc=cn&idx=")
+                string strDomain = IniHelper.IniRead("BingApiUrl", "url2", ShareClass._iniFilePath, "www.bing.com");
+                string strUrl = IniHelper.IniRead("BingApiUrl", "url1", ShareClass._iniFilePath, "https://")
+                              + strDomain
+                              + IniHelper.IniRead("BingApiUrl", "url3", ShareClass._iniFilePath, "/HPImageArchive.aspx?format=js&cc=cn&idx=")
                               + intIDX.ToString()
-                              + IniHelper.IniRead("BingApiUrl", "url2", ShareClass._iniFilePath, "&n=")
+                              + IniHelper.IniRead("BingApiUrl", "url4", ShareClass._iniFilePath, "&n=")
                               + intN.ToString()
-                              + IniHelper.IniRead("BingApiUrl", "url3", ShareClass._iniFilePath, "&video=1");
+                              + IniHelper.IniRead("BingApiUrl", "url5", ShareClass._iniFilePath, "&video=1");
                 bool isUHD = true;
                 if (IniHelper.IniRead("DownloadSetting", "PixelResolution", ShareClass._iniFilePath, "UHD") != "UHD")
                 {
                     isUHD = false;
                 }
-                string strJson = HttpHelper.Get(strUrl);
-                Dictionary<string, List<string>> dicEveryDayInformation = ShareClass.GetJsonToBing(strJson, isUHD);
                 string fileNameLanguageIsEnglish = IniHelper.IniRead("DownloadSetting", "FileNameLanguageIsEnglish", ShareClass._iniFilePath, "true");
                 int intLanguage;
                 if (fileNameLanguageIsEnglish == "true")
@@ -100,29 +110,102 @@ namespace BingImageDownload
                 {
                     Directory.CreateDirectory(strPath);
                 }
-                Console.WriteLine("配置文件读取完毕，开始下载文件……");
-                string strStatus = string.Empty;
-                foreach (List<string> item in dicEveryDayInformation.Values)
+                int intNetWaitTime;
+                intNetWaitTime = Int32.TryParse(IniHelper.IniRead("NetworkInformation", "NetWaitTime", ShareClass._iniFilePath, "2000"), out intNetWaitTime) ? intNetWaitTime : 2000;
+                int intNetRetryCount;
+                intNetRetryCount = Int32.TryParse(IniHelper.IniRead("NetworkInformation", "NetRetryCount", ShareClass._iniFilePath, "5"), out intNetRetryCount) ? intNetRetryCount : 5;
+                int intExitTime;
+                intExitTime = Int32.TryParse(IniHelper.IniRead("OtherSetting", "ExitTime", ShareClass._iniFilePath, "3000"), out intExitTime) ? intExitTime : 3000;
+                Console.WriteLine("配置文件读取完毕，开始测试网络连接……");
+                bool isSuccess = false;
+                int intCount = 0;
+                while (!isSuccess && intCount < intNetRetryCount)
                 {
-                    Console.WriteLine(item[3]);
-                    ShareClass.DownloadFile(item[0], Path.Combine(strPath, (item[3] + " " + item[intLanguage])));
-                    if (dicEveryDayInformation.Count > 1)
+                    Ping pingSender = new Ping();
+                    PingReply pingReply = null;
+                    try
                     {
-                        Thread.Sleep(5000);
+                        pingReply = pingSender.Send(strDomain, 1000);
                     }
-                    strStatus = $"文件 ：{item[intLanguage]}\n下载完成." + item[0];
-                    LogHelper.Log(strStatus);
-                    Console.WriteLine(strStatus);
+                    catch (Exception e1)
+                    {
+                        LogHelper.GetExceptionLocation(e1);
+                    }
+                    finally
+                    {
+                        if (pingReply == null || (pingReply != null && pingReply.Status != IPStatus.Success))
+                        {
+                            Console.WriteLine($"无法连接，请检查网络。重新尝试{(intCount + 1).ToString()}");
+                            intCount++;
+                            Thread.Sleep(intNetWaitTime);
+                        }
+                        else if (pingReply != null && pingReply.Status == IPStatus.Success)
+                        {
+                            Console.WriteLine("连接成功");
+                            isSuccess = true;
+                        }
+                    }
                 }
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("【版权】仅限于壁纸使用。它们是受版权保护的图像，因此您不应将其用于其他目的，但可以将其用作桌面壁纸。");
-                Console.ForegroundColor = default;
-                Console.WriteLine();
-                Console.WriteLine("文件下载完毕，请到你指定的文件夹中查看，程序即将退出。");
-                int intWait;
-                intWait = Int32.TryParse(IniHelper.IniRead("OtherSetting", "ExitTime", ShareClass._iniFilePath, "3000"), out intWait) ? intWait : 3000;
-                Thread.Sleep(intWait);
+                if (intCount > 0 && intCount >= intNetRetryCount)
+                {
+                    Console.WriteLine($"无法访问 {strDomain} ，请检查网络。");
+                    isSuccess = false;
+                }
+                else
+                {
+                    Console.WriteLine("网络连接通畅，开始下载文件……");
+                    string strJson = HttpHelper.Get(strUrl);
+                    Dictionary<string, List<string>> dicEveryDayInformation = ShareClass.GetJsonToBing(strJson, isUHD);
+                    string strStatus = string.Empty;
+                    isSuccess = dicEveryDayInformation.Count < 1 ? false : isSuccess;
+                    foreach (List<string> item in dicEveryDayInformation.Values)
+                    {
+                        try
+                        {
+                            Console.WriteLine(item[3]);
+                            ShareClass.DownloadFile(item[0], Path.Combine(strPath, (item[3] + " " + item[intLanguage])));
+                            if (dicEveryDayInformation.Count > 1)
+                            {
+                                Thread.Sleep(5000);
+                            }
+                            strStatus = $"文件 ：{item[intLanguage]}\n下载完成." + item[0];
+                            LogHelper.Log(strStatus);
+                            Console.WriteLine(strStatus);
+                            isSuccess = true;
+                        }
+                        catch (Exception e1)
+                        {
+                            LogHelper.GetExceptionLocation(e1);
+                            isSuccess = false;
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("文件下载出错，请到程序目录的Log文件中查看日志文件，分析出错原因。");
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                }
+
+                if (isSuccess)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("【版权】仅限于壁纸使用。它们是受版权保护的图像，因此您不应将其用于其他目的，但可以将其用作桌面壁纸。");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("文件下载完毕，请到你指定的文件夹中查看。");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("文件下载出错，请到程序目录的Log文件中查看日志文件，分析出错原因。");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine("程序即将退出");
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(intExitTime);
                 if (IniHelper.IniRead("OtherSetting", "AutoExit", ShareClass._iniFilePath) == "1")
                 {
                     Console.WriteLine("配置文件中设置了自动退出。");
